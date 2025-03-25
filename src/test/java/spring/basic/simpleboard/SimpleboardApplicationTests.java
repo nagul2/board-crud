39895b9014f89fc443c94e0350d94a4fc9432f05
nagul2 <hayani0000@gmail.com>
2025-03-25 17:42:45 +0900

    Setting: 프로젝트 생성

---
+++ src/test/java/spring/basic/simpleboard/SimpleboardApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.basic.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
